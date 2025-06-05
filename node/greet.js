12af87c010e93de7c440cb6b900226cf41aaa9a6
0anant <bigbluemagic06@gmail.com>
2025-06-05 08:40:39 -0700

    little abt node.js

---
+++ node/greet.js
@@ -0,0 +1,9 @@
+const greet = "Praveen sen";
+
+module.exports = greet;
+
+const greet2 = "Rinki sen";
+const greet3 = "Nisha sen";
+const greet4 = "Priyanka sen";
+
+module.exports =  {greet2, greet3, greet4}; 
---
+++ node/index.js
@@ -0,0 +1,16 @@
+console.log("Hello");   `// This is a simple Node.js script that prints "Hello" to the console.`
+
+// module wrappers
+
+console.log(__filename);
+console.log(__dirname);
+
+//Modules --it is a way to pass our code from one file to another file
+
+const greet = require("./greet");
+console.log(greet); // This will print the value exported from greet.js
+
+const {greet2, greet3, greet4} = require("./greet");
+console.log(greet2); // This will print the value of greet2 exported from greet.js
+console.log(greet3); // This will print the value of greet3 exported from greet.js
+console.log(greet4); // This will print the value of greet4 exported from greet.js
