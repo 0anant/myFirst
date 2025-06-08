8d81d9ceb1b66a2bc1c58d00bb0454176d90b6ad
0anant <bigbluemagic06@gmail.com>
2025-06-08 15:20:16 +0530

    jsx rules file

---
+++ REACT/vite-project/src/JSXRules/JSXRules.jsx
@@ -0,0 +1,20 @@
+const JSXRules = () => {
+  return (
+    <div>
+        <h1>
+            "JSX Rules"
+        </h1>
+        <p>
+        <ol>
+            <li>JSX must return a single parent</li>
+            <li>JSX elements must be properly closed</li>
+            <li>JSX attributes are written using camel case</li>
+            <li>in JSX instead of using for, use htmlFor in Forms(labels)</li>
+            <li>in JSX instesd of using class, className instead</li>
+        </ol>
+        </p>
+    </div>
+  )
+}
+
+export default JSXRules
